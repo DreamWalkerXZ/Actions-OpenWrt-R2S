56ab47b74a320582d32785947ed0701480a833e6
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2022-06-12 23:31:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/R2S/g' package/base-files/files/bin/config_generate
